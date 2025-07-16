e860ca2ddb9e9ebb43619988898aaaf2128f9186
Brad Howes <bradhowes@mac.com>
2025-07-16 18:32:57 +0200

    New base class for common behavior

---
+++ Sources/AUv3Component/AudioUnitViewControllerBase.swift
@@ -0,0 +1,85 @@
+import AUv3Shared
+import Combine
+import CoreAudioKit
+import os
+import SwiftUI
+
+/**
+ Protocol for a factory of AUv3HostingController instances
+ */
+@MainActor
+public protocol HostingControllerFactory {
+  associatedtype AUv3View: View
+
+  /**
+   Create a new AUv3HostingController and its hosted AUv3 view.
+
+   - parameter audioUnit: the audio unit to show in the hosted view
+   - returns: new AUv3HostingController instance
+   */
+  static func make(audioUnit: FilterAudioUnit) -> AUv3HostingController<AUv3View>
+}
+
+/**
+ Generic base controller class that hosts AUv3 SwiftUI views. Delegates creation of the hosting view controller to the
+ parameter factory function. Example use:
+
+ ```
+ @MainActor
+ struct ViewControllerFactory: HostingControllerFactory {
+   static func make(audioUnit: FilterAudioUnit) -> AUv3HostingController<AUMainView> {
+     guard let parameterTree = audioUnit.parameterTree,
+           let gain = parameterTree.parameter(withAddress: InteropPlay2AU_ParameterAddress.gain.rawValue)
+     else {
+       fatalError("failed to get gain parameter")
+     }
+
+     return .init(rootView: .init(gain: gain))
+   }
+ }
+
+ @MainActor
+ class AudioUnitViewController: AudioUnitViewControllerBase<ViewControllerFactory> {}
+ ```
+ */
+@MainActor
+open class AudioUnitViewControllerBase<HCF: HostingControllerFactory>: AUViewController {
+  public var hostingController: AUv3HostingController<HCF.AUv3View>?
+
+  public var audioUnit: FilterAudioUnit? {
+    didSet {
+      if self.isViewLoaded,
+         let audioUnit = self.audioUnit {
+        self.configureSwiftUIView(audioUnit: audioUnit)
+      }
+    }
+  }
+
+  private func configureSwiftUIView(audioUnit: FilterAudioUnit) {
+    if let host = hostingController {
+      host.removeFromParent()
+      host.view.removeFromSuperview()
+    }
+
+    let host = HCF.make(audioUnit: audioUnit)
+
+#if os(macOS)
+    host.view.wantsLayer = true
+    // host.view.layer?.backgroundColor = NSColor.red.cgColor
+#endif
+
+    self.addChild(host)
+    host.view.frame = self.view.bounds
+    self.view.addSubview(host.view)
+    hostingController = host
+
+    // Make sure the SwiftUI view fills the full area provided by the view controller
+    host.view.translatesAutoresizingMaskIntoConstraints = false
+    host.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+    host.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+    host.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+    host.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    self.view.bringSubviewToFront(host.view)
+  }
+}
+
